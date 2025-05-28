cf302ef9f4ee691bd5064e16820d19b77ecd9be5
tiiyaaraa <tfitri239@gmail.com>
2025-05-28 21:18:06 +0700

    Update app.py

--- Deploy Capstone/app.py
+++ Deploy Capstone/app.py
@@ -12,7 +12,7 @@ CORS(app, origins=["*"], supports_credentials=True)
 
 @app.after_request
 def add_cors_headers(response):
-    response.headers["Access-Control-Allow-Origin"] = "http://localhost:5173"
+    response.headers["Access-Control-Allow-Origin"] = "*"
     response.headers["Access-Control-Allow-Methods"] = "GET, POST"
     response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
     response.headers["Access-Control-Allow-Credentials"] = "true"
